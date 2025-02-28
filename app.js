31081e5c83e5ddea0ddf3f496a7c282e3e69a039
Jirosvi <jirosvi99@gmail.com>
2025-02-27 20:19:44 -0500

    funcion limpiar

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de
+// programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let obtenerNombre = document.getElementById('amigo');
+    let nombre = obtenerNombre.value.trim();
+
+    if(nombre != ''){
+        amigos.push(nombre);
+        limpiar();
+}
+
+function limpiar(){
+    document.querySelector('#amigo').value = '';
+}
