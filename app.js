eb1c6f4f99f25beeba0cf6c9ccf9bf97aafc4a42
Jirosvi <jirosvi99@gmail.com>
2025-02-28 13:42:49 -0500

    Terminando el proyecto

--- app.js
+++ app.js
@@ -1,16 +1,48 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de
-// programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
+let obtenerNombre = document.getElementById('amigo');
+
 
 function agregarAmigo() {
-    let obtenerNombre = document.getElementById('amigo');
     let nombre = obtenerNombre.value.trim();
 
-    if(nombre != ''){
-        amigos.push(nombre);
-        limpiar();
+    if(nombre !== '') {
+        if(!amigos.includes(nombre)){
+            amigos.push(nombre);
+            console.log(amigos);
+            actualizarLista();
+            limpiar();
+        } else {
+            alert('El nombre ya existe');
+        }
+    }else{
+        alert('Por favor, inserte un nombre');
+    }
 }
 
+//Función para limpiar caja
 function limpiar(){
     document.querySelector('#amigo').value = '';
+}
+
+function actualizarLista(){
+    let listaNombres = document.querySelector('#listaAmigos');
+    listaNombres.innerHTML = '';
+
+    for(let i = 0; i < amigos.length; i++){
+        let nombre = amigos[i];
+        listaNombres.innerHTML += `<li>${nombre}</li>`;
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length > 0) {
+        // Generar un índice aleatorio basado en la longitud del array de amigos
+        const numeroAleatorio = Math.floor(Math.random() * amigos.length);
+        const nombreSorteado = amigos[numeroAleatorio];
+
+        // Mostrar el nombre sorteado en algún lugar de la página
+        document.getElementById('resultado').textContent = `Nombre sorteado: ${nombreSorteado}`;
+    } else {
+        alert('No hay nombres en la lista para sortear.');
+    }
 }
